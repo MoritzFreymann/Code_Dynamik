7f54e3a4998746b13ea315f197d5a647b6ee1623
MoritzFreymann <32904684+MoritzFreymann@users.noreply.github.com>
2017-12-05 10:27:54 +0100

    id

--- berechne_dk_geschwindigkeiten.m
+++ berechne_dk_geschwindigkeiten.m
@@ -18,6 +18,7 @@ function rob = berechne_dk_geschwindigkeiten(rob)
         % Absolute Translationsgeschwindigkeit berechnen
         % ----------------------------------------------
         rob.kl(i).Bi_dot_r_i = 0;   % auschliesslich Rotation
+        
     % Berechnung fuer restliche Koerper
     for i = 2:rob.N_Q
         % Index des Vorgaengers merken
@@ -25,11 +26,13 @@ function rob = berechne_dk_geschwindigkeiten(rob)
 
         % Relative Winkelgeschwindigkeit berechnen
         % ----------------------------------------
-        rob.kl(i).Bi_omega_rel = Ax( -rob.kl(i).alpha) * [ 0; 0; rob.dot_q(i) ];    % = v_omega_v,i
+        rob.kl(i).Bi_omega_rel = Ax( -rob.kl(i).alpha) * [ 0; 0; rob.dot_q(i) ];    
+        % = v_omega_v,i
 
         % Absolute Winkelgeschwindigkeit berechnen
         % ----------------------------------------
-        rob.kl(i).Bi_omega = rob.kl(i).A_iv * ( rob.kl(vor).Bi_omega + rob.kl(i).Bi_omega_rel ); % i_omega_i = i_A_v * ( v_omega_v + v_omega_v,i )
+        rob.kl(i).Bi_omega = rob.kl(i).A_iv * ( rob.kl(vor).Bi_omega + rob.kl(i).Bi_omega_rel ); 
+        % i_omega_i = i_A_v * ( v_omega_v + v_omega_v,i )
 
         % Absolute Translationsgeschwindigkeit berechnen
         % ----------------------------------------------
